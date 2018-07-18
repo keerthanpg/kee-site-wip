4ce3097332b818687184f910fea3b3a33c41754e
nuwandavek <vivekaithal44@gmail.com>
2018-07-19 00:58:07 +0530

    [Vivek] remove more unwanted files

---
+++ project_data.js
@@ -0,0 +1,23 @@
+var projects= [
+	{	
+		'id' : 'projects',
+		'entities' :  [
+		{	
+			'id' : 'P0',
+			'name' : 'The Anatomy of a Debate',
+			'image' : 'res/projects/sio.jpg',
+			'subhead' : 'Analysis of a debate show on India\'s most popular English News Channel',
+			'description' : "\"Make no mistake, this is no debate show. It is WWE. It is entertainment. It is a Mortal Kombat cage match between a trishool wielding bhakt and a commie-sympathizing-sikular. And it makes for really great TV.\" <br> This was an effort to analyze the debate show on India's most popular news channel in order to break it down to its most fundamental traits. This frustrating show effectively uses confusion and quarrel to establish false equivalencies and muddy the water, in any given debate. The main idea was to put numbers behind anecdotal claims so as to inspire a deeper study about the nature of 'Debate Shows' on Indian TV. Despite not being statistically significant, deep-diving into one debate efficiently brings forth the ideas to be further explored in an aggregate analysis.<br><br> <code>#chrome-extension #d3.js #javascript #html #css </code>",
+			'demoLink' : 'http://vivekaithal.co/shoutitout/',
+			'codeLink' : 'https://github.com/nuwandavek/shoutitout',
+		},{	
+			'id' : 'P1',
+			'name' : 'Suitup 2015',
+			'image' : 'res/projects/suitup.jpg',
+			'subhead' : 'Analysis of IIT Kharagpur Placement data',
+            'description' : "This was a month long project, in association with the Career Development Center of IIT Kharagpur, to analyze and visualize job placement data of 2015. The final aim was to get insights that may help both the team, and the students do a better job, in the following year. A survey was conducted, right after placements and the results of that were incorporated into this project as well. A detailed presentation was given to the Director and Deans, based on findings.<br><br> <code>#python #d3.js #javascript #html #css </code>",
+			'demoLink' : 'http://thescholarsavenue.github.io/suitup15/',
+			'codeLink' : 'https://github.com/thescholarsavenue/suitup15',
+		}]
+	}
+]
