9db304d8ee4ebb6673308cb979bd8bace8e6d392
Keerthana P G; RDNA/AD <keerthana.p_g@daimler.com>
2018-07-21 19:07:58 -0700

    update json

--- project_data.js
+++ project_data.js
@@ -1,17 +1,41 @@
 var projects= [
-	{	
+	{
 		'id' : 'projects',
 		'entities' :  [
-		{	
+		{
 			'id' : 'P0',
-			'name' : 'The Anatomy of a Debate',
-			'image' : 'res/projects/sio.jpg',
-			'subhead' : 'Analysis of a debate show on India\'s most popular English News Channel',
-			'description' : "\"Make no mistake, this is no debate show. It is WWE. It is entertainment. It is a Mortal Kombat cage match between a trishool wielding bhakt and a commie-sympathizing-sikular. And it makes for really great TV.\" <br> This was an effort to analyze the debate show on India's most popular news channel in order to break it down to its most fundamental traits. This frustrating show effectively uses confusion and quarrel to establish false equivalencies and muddy the water, in any given debate. The main idea was to put numbers behind anecdotal claims so as to inspire a deeper study about the nature of 'Debate Shows' on Indian TV. Despite not being statistically significant, deep-diving into one debate efficiently brings forth the ideas to be further explored in an aggregate analysis.<br><br> <code>#chrome-extension #d3.js #javascript #html #css </code>",
-			'demoLink' : 'http://vivekaithal.co/shoutitout/',
-			'codeLink' : 'https://github.com/nuwandavek/shoutitout',
-		},{	
+			'name' : 'AutoPulse',
+			'image' : 'res/projects/suitup.jpg',
+			'subhead' : 'Finalist at World Blockchain Hackathon, San Francisco, July 2018',
+            'description' : "This is a decentralised app based on the ethreum framework that proposes to improve privacy of car owners, help government and law enforcement manage vehicles for violations, safety/pollution standards and dealers/manufacturers with maintenance and servicing. The core idea is achieved by removing the license places and transmitting a dynamic ID via QR code or bluetooth becaons on car which can only be encrypted by authorised parties. This allows for irrefutable ownership and history of cars as well which is of value in user car sales market.<br><br> <code>#python #d3.js #javascript #html #css </code>",
+			'reportLink' : 'https://docs.google.com/presentation/d/1HG1xXLB_KTHT5bFT4plJwzJKtlSCwIT6F_nScdoOXmI/edit?usp=sharing',
+			'codeLink' : 'https://github.com/keerthanpg/Hackathon_Vehicle_ID',
+		}, {
 			'id' : 'P1',
+			'name' : 'Efficient Communication among Multi-Agents using Shared Memory',
+			'image' : 'img/sc2.jpg',
+			'subhead' : 'Asynchronous Advantageous agents on StarCraft 2',
+			'description' : "\"Make no mistake, this is no debate show. It is WWE. It is entertainment. It is a Mortal Kombat cage match between a trishool wielding bhakt and a commie-sympathizing-sikular. And it makes for really great TV.\" <br> This was an effort to analyze the debate show on India's most popular news channel in order to break it down to its most fundamental traits. This frustrating show effectively uses confusion and quarrel to establish false equivalencies and muddy the water, in any given debate. The main idea was to put numbers behind anecdotal claims so as to inspire a deeper study about the nature of 'Debate Shows' on Indian TV. Despite not being statistically significant, deep-diving into one debate efficiently brings forth the ideas to be further explored in an aggregate analysis.<br><br> <code>#chrome-extension #d3.js #javascript #html #css </code>",
+			'reportLink' : 'http://vivekaithal.co/shoutitout/',
+			'videoLink' : 'https://www.youtube.com/watch?v=6TnkOidvieM&index=3&list=PL4qVRWUWbgR7WxD6U86U79xMcuoxAl6hh',
+		}, {
+			'id' : 'P2',
+			'name' : 'Multimodal Emotion Recognition',
+			'image' : 'img/mme.jpg',
+			'subhead' : 'Emotion Recognition using Deep Learning',
+            'description' : "This was a month long project, in association with the Career Development Center of IIT Kharagpur, to analyze and visualize job placement data of 2015. The final aim was to get insights that may help both the team, and the students do a better job, in the following year. A survey was conducted, right after placements and the results of that were incorporated into this project as well. A detailed presentation was given to the Director and Deans, based on findings.<br><br> <code>#python #d3.js #javascript #html #css </code>",
+			'websiteLink' : 'https://mrsdprojects.ri.cmu.edu/2017teamd/summary/',
+			'codeLink' : 'https://github.com/keerthanpg/Multimodal-Emotion-Recognition', 'https://github.com/keerthanpg/Analysis-of-CMU-MOSI',
+		}, {
+			'id' : 'P3',
+			'name' : 'Suitup 2015',
+			'image' : 'res/projects/suitup.jpg',
+			'subhead' : 'Analysis of IIT Kharagpur Placement data',
+            'description' : "This was a month long project, in association with the Career Development Center of IIT Kharagpur, to analyze and visualize job placement data of 2015. The final aim was to get insights that may help both the team, and the students do a better job, in the following year. A survey was conducted, right after placements and the results of that were incorporated into this project as well. A detailed presentation was given to the Director and Deans, based on findings.<br><br> <code>#python #d3.js #javascript #html #css </code>",
+			'demoLink' : 'http://thescholarsavenue.github.io/suitup15/',
+			'codeLink' : 'https://github.com/thescholarsavenue/suitup15',
+		}, {
+			'id' : 'P4',
 			'name' : 'Suitup 2015',
 			'image' : 'res/projects/suitup.jpg',
 			'subhead' : 'Analysis of IIT Kharagpur Placement data',
